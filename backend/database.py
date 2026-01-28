5e5a6f23ba084b9ca5948ec9e6b9c476eb1e3b7b
Ardavannafezi <ardavannafezi@gmail.com>
2026-01-28 14:01:12 -0800

    v0.3.1

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from passlib.context import CryptContext
 
 from data.countries import COUNTRIES
